70670ee96cefa5eeb8db0a140056b66d8dbb0690
brCreate <117453864+brCreate@users.noreply.github.com>
2025-07-07 16:46:47 +0800

    Fixes few issues, version 0.7.2

--- src/plmap.h
+++ src/plmap.h
@@ -16,7 +16,7 @@ bool pl_mapGet(const PlMap *hash, PlValue key, PlValue *value);
 bool pl_mapSet(PlMap *hash, PlValue key, PlValue value);
 bool pl_mapDel(const PlMap *hash, PlValue key);
 void pl_mapAdd(const PlMap *from, PlMap *to);
-PlString *pl_mapFindString(PlMap *map, const char *chars, int length, uint32_t hash);
+PlString *pl_mapFindString(const PlMap *map, const char *chars, int length, uint32_t hash);
 
 void pl_mapRemoveWhite(const PlMap *map);
 void pl_markMap(const PlMap *map);
